18906ad6c16eeebdbea0b71c5a48d7fc49d95548
Dyllan Hope <dyllanjhope@gmail.com>
2019-10-14 10:00:38 +0200

    added further testing

--- test/waiter-manager.test.ts
+++ test/waiter-manager.test.ts
@@ -5,10 +5,12 @@ import { WaiterService } from "../src/waiter-service";
 import { Waiters } from "../src/entity/Waiters";
 import { Shifts } from '../src/entity/Shifts';
 
-describe('Waiter shift app with TypeORM tests', async () => {
+describe('Waiter shift app with TypeORM service tests', async () => {
     before(async function () {
         await createConnection({
             "type": "postgres",
+            "username": "coder",
+            "password": "pg123",
             "database": "waiter_shifts",
             "synchronize": true,
             "logging": false,
@@ -31,7 +33,6 @@ describe('Waiter shift app with TypeORM tests', async () => {
             const results = await Waiters.find();
             assert.equal(results[0].firstName, "Dyllan");
         })
-
         it('Should return that Dyllan & John were both loaded into the waiters Database', async () => {
             const waiterService = new WaiterService();
             await waiterService.clearWeekdayTable();
@@ -44,6 +45,32 @@ describe('Waiter shift app with TypeORM tests', async () => {
             const results = await Waiters.find();
             assert.equal(results[0].firstName, "John");
             assert.equal(results[1].firstName, "Mike");
+        })
+    })
+    describe('Testing The shifting of waiters in the Database', () => {
+        it('Should return that Vuyo is shifted for Monday, Thursday & Saturday', async () => {
+            const waiterService = new WaiterService();
+            await waiterService.clearWeekdayTable();
+            await waiterService.clearWaitersTable();
+            await waiterService.loadWeekdays();
+
+            await waiterService.addWaiter('Vuyo', 'Matu', '123');
+
+            await waiterService.shiftWaiter('Vuyo', ['Monday', 'Thursday', 'Saturday']);
+            assert.deepEqual(await waiterService.displayShiftsForWaiter("Vuyo"), ['Monday', 'Thursday', 'Saturday']);
+        })
+        it('Should return that John & Dyllan are both shifted for Monday', async () => {
+            const waiterService = new WaiterService();
+            await waiterService.clearWeekdayTable();
+            await waiterService.clearWaitersTable();
+            await waiterService.loadWeekdays();
+
+            await waiterService.addWaiter('Dyllan', 'Hope', '123');
+            await waiterService.addWaiter('John', 'Hope', '123');
+
+            await waiterService.shiftWaiter('John', ['Monday', 'Thursday', 'Saturday']);
+            await waiterService.shiftWaiter('Dyllan', ['Monday', 'Wednesday', 'Friday']);
+            assert.deepEqual(await waiterService.displayWaitersForShift("Monday"), ['John', 'Dyllan']);
         })
     })
 });
